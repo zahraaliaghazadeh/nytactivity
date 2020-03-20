73fcb84f91f44e92ca6ea66b66f176b23a6b6514
Megan Jacobs <meganjacobs@live.com>
2020-03-20 13:37:19 -0700

    added initial variables and ajax call

---
+++ script.js
@@ -0,0 +1,12 @@
+var queryURL = ""; 
+var apiKey = "5d9e2294-8fed-4b9d-ac40-b10721c8e6e9"; 
+var searchTerm; 
+var numResults; 
+var startYear; 
+var endYear; 
+
+$.ajax({
+
+}).then(function(response) {
+
+}); 
